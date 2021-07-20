118b0b195e7dfb04e4375b84f8a593d6dc29a4e3
clarice_r <claricerdallorto@gmail.com>
2021-07-20 10:39:56 +0100

    Set up api request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 // import './App.css';
+import React, { useState, useEffect } from "react";
+import PokeList from "./PokeList";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
+    setPokemon(res.data.results.map(p => p.name)) 
+  })
+  }, [])
+  
   return (
-    null
+    <PokeList pokemon={pokemon}/>
   );
 }
 
